256698bfed9ba61f2346c72baa55502fc45a46ab
Magnus Klingenberg <klingenberg.magnus@gmail.com>
2016-04-05 11:38:48 +0200

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,7 +11,7 @@
 
 class loggly::params {
   case $operatingsystem {
-    'RedHat', 'Ubuntu', 'Fedora', 'CentOS', 'Debian': {
+    'RedHat', 'Ubuntu', 'Fedora', 'CentOS', 'Debian', 'OracleLinux': {
       # base directory for loggly support files
       $base_dir = '/usr/local/loggly'
 
